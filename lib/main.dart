c37250b9cc5d9cfea13221f37b1c78637c9e3b43
Phonekham <phone.madridista@gmail.com>
2022-01-02 12:40:33 +0700

    product overview & item

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,25 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a shop!'),
-      ),
-    );
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ProductOverviewScreen());
   }
 }
