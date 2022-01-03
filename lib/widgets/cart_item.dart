04b418d182ff0a8ab6b6fa21ce0eccb548b2ee60
Phonekham <phone.madridista@gmail.com>
2022-01-03 11:24:00 +0700

    show dialog on remove cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -37,6 +37,26 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text('Are you sure'),
+                  content: Text('Do you want to remove item from the cart?'),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(false);
+                        },
+                        child: Text('No')),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(true);
+                        },
+                        child: Text('Yes'))
+                  ],
+                ));
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
